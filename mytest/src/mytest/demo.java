a2cf197b7eade32c14bc03b0cf40c353e8496f72
penggaibian <1595937121@qq.com>
2019-04-12 18:40:57 +0800

    this test

--- mytest/src/mytest/demo.java
+++ mytest/src/mytest/demo.java
@@ -4,6 +4,6 @@ public class demo {
 	public static void main(String[] args) {
 		System.out.println("this is  a java demo");
 		System.out.println("dsfsdfsfsdfs");
-		System.out.println("dfsfsdfgdgdfgfdgfg");
+		
 	}
 }
